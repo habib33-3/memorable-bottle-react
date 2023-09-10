5bafb1b475da27025cbfd595e2342096ea03368b
Habibur Rahman <habib.swi16@gmail.com>
2023-09-10 22:58:48 +0600

    addToCart added

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,12 +1,20 @@
-const Bottle = ({ bottle }) => {
+const Bottle = ({ bottle, handleAddToCart }) => {
   const { name, price, img } = bottle;
 
   return (
-    <div className="flex flex-col items-center justify-center card w-96 shadow-xl border-[#2E8B57] bg-base-200 p-2 space-y-3 rounded-2xl">
-      <h3 className="text-2xl font-semibold">Bottle {name}</h3>
-      <img className="w-52" src={img} alt="" />
-      <p>Price: ${price}</p>
-    </div>
+    <>
+      <div className="flex flex-col items-center justify-center card w-96 shadow-xl border-[#2E8B57] bg-base-200 p-2 space-y-3 rounded-2xl">
+        <h3 className="text-2xl font-semibold">Bottle {name}</h3>
+        <img className="w-52" src={img} alt="" />
+        <p>Price: ${price}</p>
+        <button
+          onClick={() => handleAddToCart(bottle)}
+          className="btn btn-success"
+        >
+          Purchase
+        </button>
+      </div>
+    </>
   );
 };
 
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -4,6 +4,8 @@ import Bottle from "./../Bottle/Bottle";
 const Bottles = () => {
   const [bottles, setBottles] = useState([]);
 
+  const [cart, setCart] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch("bottles.json");
@@ -14,12 +16,24 @@ const Bottles = () => {
     fetchData();
   }, []);
 
+  const handleAddToCart = (bottle) => {
+    const newCart = [...cart, bottle];
+    setCart(newCart);
+  };
+
   return (
     <div className="w-max mx-auto my-3">
       <h2 className="text-2xl">Bottles Here {bottles.length}</h2>
+      <h2>Cart {cart.length}</h2>
       <div className="grid grid-cols-3 items-center justify-center gap-10 my-10">
         {bottles.map((bottle) => {
-          return <Bottle key={bottle.id} bottle={bottle}></Bottle>;
+          return (
+            <Bottle
+              key={bottle.id}
+              bottle={bottle}
+              handleAddToCart={handleAddToCart}
+            ></Bottle>
+          );
         })}
       </div>
     </div>
