84a5e8b2c7320e35c4ecf6f46eb2a33d6e57c9e4
Habibur Rahman <habib.swi16@gmail.com>
2023-09-10 23:47:54 +0600

    build local storage integration function

---
+++ src/util/localStorage.js
@@ -0,0 +1,22 @@
+const getStoredCart = () => {
+  const storedCartString = localStorage.getItem("cart");
+
+  if (storedCartString) {
+    return JSON.parse(storedCartString);
+  }
+  return [];
+};
+
+const saveToLS = (cart) => {
+  const cartStringify = JSON.stringify(cart);
+  localStorage.setItem("cart", cartStringify);
+};
+
+const addToLS = (id) => {
+  const cart = getStoredCart();
+  cart.push(id);
+
+  saveToLS(cart);
+};
+
+export { addToLS };
